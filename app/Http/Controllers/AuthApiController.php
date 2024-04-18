ee1100805aa56fe173886b075b98aa746f5f31df
ranjushrestha08 <ranjushrestha152@gmail.com>
2024-04-18 07:40:45 +0545

    Fix error message for auth api

--- app/Http/Controllers/AuthApiController.php
+++ app/Http/Controllers/AuthApiController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator; 
+use Illuminate\Support\Facades\Validator;
 
 class AuthApiController extends Controller
 {
@@ -13,19 +13,19 @@ class AuthApiController extends Controller
     {
         // Validation logic here
 
-       $validator = Validator::make($request->all(), [
-        'name' => 'required|string',
-        'email' => 'required|email|unique:users,email',
-        'phone' => 'required|string|unique:users,phone',
-        'password' => 'required|min:8',
-        'role' => 'required|string'
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'phone' => 'required|string|unique:users,phone',
+            'password' => 'required|min:8',
+            'role' => 'required|string'
         ]);
 
-         if ($validator->fails()) {
-        return response()->json(['errors' => $validator->errors()], 422);
-    }
+        if ($validator->fails()) {
+            return response()->json(['status' => false, 'message' => $validator->errors()]);
+        }
 
- $user = User::create([
+        $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'phone' => $request->phone,
@@ -35,65 +35,74 @@ class AuthApiController extends Controller
 
         $token = $user->createToken('auth_token')->accessToken;
 
-        
+        $user['token'] = $token;
 
-        return response()->json([ 'status' => "success", 'data' => $user, 'token' => $token], 200);
+        return response()->json(['status' => true, 'data' => $user], 200);
     }
 
     public function login(Request $request)
     {
+        $validate = $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
 
-        $user = [];
+        try {
+            if (Auth::attempt(['email' => $request->input('email'), 'password' => $request->input('password')])) {
+                $user = Auth::user();
+                $token = $user->createToken('auth_token')->accessToken;
 
-        if (Auth::attempt(['email' => $request->input('email'),'password' => $request->input('password')])) {
-            $user = Auth::user();
-            $token = $user->createToken('auth_token')->accessToken;
+                return response()->json(['status' => true, 'data' => $user, 'token' => $token], 200);
+            }
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        } catch
+        (\Exception $e) {
+            return $this->sendError($e->getMessage());
+        }
 
-
-
-         return response()->json([ 'status' => "success", 'data' => $user, 'token' => $token], 200);
-        }return response()->json(['message' => 'Invalid credentials'], 401);
     }
 
     public function logout(Request $request)
     {
         $request->user()->token()->revoke();
 
-        return response()->json(['message' => 'Logged out', 'status' => "success"]);
+        return response()->json(['message' => 'Logged out', 'status' => true]);
     }
 
     public function user(Request $request)
     {
         $user = auth('api')->user();
-        return response()->json(['user' => $user, 'status' => "success"]);
+        return response()->json(['data' => $user, 'status' => true]);
     }
+
     public function updateProfile(Request $request)
     {
         $request->validate([
             //add validation here
-        ]);     
+        ]);
         $user = auth('api')->user();
         $data = $request->all();
         $user->update($data);
-        return response()->json(['user' => $user, 'status' => "success"]);
+        return response()->json(['data' => $user, 'status' => true]);
     }
 
-   public function forgetPassword(Request $request){
-    $user = User::where('email', $request->email)->first();
-            if (!$user) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Invalid User',
-                ],);
-            } else {
+    public function forgetPassword(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Invalid User',
+            ],);
+        } else {
 
-                $user ->password = bcrypt($request->new_password);
-                $user ->update();
+            $user->password = bcrypt($request->new_password);
+            $user->update();
 
-                return response()->json([
-                    'status' => true,
-                    'message' => 'Password Reset Successfully.'
-                ]);
-            }
-   }
+            return response()->json([
+                'status' => true,
+                'message' => 'Password Reset Successfully.'
+            ]);
+        }
+    }
 }
