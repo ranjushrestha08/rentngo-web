551f7bfb6c34e445be2ec87c30a98e4572292734
ranjushrestha08 <ranjushrestha152@gmail.com>
2024-04-23 21:25:21 +0545

    Update response for history

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -125,129 +125,147 @@ class ApiController extends Controller
 
     public function getUserRentals()
     {
-        $rent = Rental::with(['payment'])->where('user_id', auth('api')->user()->id)->get();
+        $rent = Rental::with(['dropLocation', 'pickLocation', 'payment'])->where('user_id', auth('api')->user()->id)->get();
 
         return response()->json([
             'status' => true,
-            'data' => $rent
+            'data' => RentalResource::collection($rent)
         ]);
 
     }
 
     public function updateRentalStatus(Request $request, $id)
     {
-        $rent = Rental::where('id', $id)->first();
-        if ($rent) {
-            if ($rent->user_id == auth('api')->user()->id) {
-                $rent->update([
-                    'rental_status' => $request->rental_status,
-                ]);
+        try {
+            $rent = Rental::where('id', $id)->first();
+            if ($rent) {
+                if ($rent->user_id == auth('api')->user()->id) {
+                    $rent->update([
+                        'rental_status' => $request->rental_status,
+                    ]);
 
-                return response()->json([
-                    'status' => true,
-                    'data' => $rent
-                ]);
+                    return response()->json([
+                        'status' => true,
+                        'data' => $rent
+                    ]);
+                } else {
+                    return response()->json([
+                        'status' => false,
+                        'message' => "Unauthorized access"
+                    ]);
+                }
             } else {
                 return response()->json([
                     'status' => false,
-                    'message' => "Unauthorized access"
+                    'message' => "Order not found"
                 ]);
             }
-        } else {
+        }catch (\Exception $ex)
+        {
             return response()->json([
                 'status' => false,
-                'message' => "Order not found"
+                'message' => $ex->getMessage()
             ]);
         }
+
     }
 
     public function updateRental(Request $request, $id)
     {
-        $data = $request->all();
-        $rent = Rental::where('id', $id)->first();
-        if ($rent) {
-            if ($rent->user_id == auth('api')->user()->id) {
 
-                if($rent->status != "Pending") {
-                    return response()->json(['status' => false, 'message' => 'This order cannot be updated']);
-                }
-                if ($request->drop) {
-                    $drop_location_name = $request->drop['location_name'];
-                    $drop_lat_lon = explode(' ', $request->drop['latlon']);
+        try {
+            $data = $request->all();
+            $rent = Rental::where('id', $id)->first();
+            if ($rent) {
+                if ($rent->user_id == auth('api')->user()->id) {
+
+                    if ($rent->status != "Pending") {
+                        return response()->json(['status' => false, 'message' => 'This order cannot be updated']);
+                    }
+                    if ($request->drop) {
+                        $drop_location_name = $request->drop['location_name'];
+                        $drop_lat_lon = explode(' ', $request->drop['latlon']);
 
 
-                    $drop_location = Location::firstOrCreate([
-                        'name' => $drop_location_name,
-                        'latitude' => $drop_lat_lon[0],
-                        'longitude' => $drop_lat_lon[1],
+                        $drop_location = Location::firstOrCreate([
+                            'name' => $drop_location_name,
+                            'latitude' => $drop_lat_lon[0],
+                            'longitude' => $drop_lat_lon[1],
+                        ]);
+
+                        $data['drop_location_id'] = $drop_location->id;
+                    }
+
+                    if ($request->pickup) {
+                        $pickup_location_name = $request->pickup['location_name'];
+                        $pickup_lat_lon = explode(' ', $request->pickup['latlon']);
+
+                        $pickup_location = Location::firstOrCreate([
+                            'name' => $pickup_location_name,
+                            'latitude' => $pickup_lat_lon[0],
+                            'longitude' => $pickup_lat_lon[1],
+                        ]);
+                        $data['pick_location_id'] = $pickup_location->id;
+
+                    }
+                    $rent->update($data);
+                    $rent = $rent->load(['vehicle', 'payment']);
+
+                    return response()->json([
+                        'status' => true,
+                        'data' => new RentalResource($rent)
                     ]);
-
-                    $data['drop_location_id'] = $drop_location->id;
-                }
-
-                if ($request->pickup) {
-                    $pickup_location_name = $request->pickup['location_name'];
-                    $pickup_lat_lon = explode(' ', $request->pickup['latlon']);
-
-                    $pickup_location = Location::firstOrCreate([
-                        'name' => $pickup_location_name,
-                        'latitude' => $pickup_lat_lon[0],
-                        'longitude' => $pickup_lat_lon[1],
+                } else {
+                    return response()->json([
+                        'status' => false,
+                        'message' => "Unauthorized access"
                     ]);
-                    $data['pick_location_id'] = $pickup_location->id;
-
                 }
-                $rent->update($data);
-                $rent = $rent->load(['vehicle', 'payment']);
-
-                return response()->json([
-                    'status' => true,
-                    'data' => new RentalResource($rent)
-                ]);
             } else {
                 return response()->json([
                     'status' => false,
-                    'message' => "Unauthorized access"
+                    'message' => "Order not found"
                 ]);
             }
-        } else {
+        }catch (\Exception $ex)
+        {
             return response()->json([
                 'status' => false,
-                'message' => "Order not found"
+                'message' => $ex->getMessage()
             ]);
         }
     }
 
-    public
-    function verifyPayment(Request $request)
-    {
-        $token = $request->token;
-        $amount = $request->amount;
-
-        $args = http_build_query(array(
-            'token' => $token,
-            'amount' => $amount
-        ));
-
-        $url = "https://khalti.com/api/v2/payment/verify/";
-
-// Make the call using API.
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $args);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        $headers = ['Authorization: test_secret_key_b76acf9c6944411c90462f5fcc220707'];
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-        // Response
-        $response = curl_exec($ch);
-        $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-
-        return $response;
-    }
+//    public
+//    function verifyPayment(Request $request)
+//    {
+//        $token = $request->token;
+//        $amount = $request->amount;
+//
+//        $args = http_build_query(array(
+//            'token' => $token,
+//            'amount' => $amount
+//        ));
+//
+//        $url = "https://khalti.com/api/v2/payment/verify/";
+//
+//// Make the call using API.
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_URL, $url);
+//        curl_setopt($ch, CURLOPT_POST, 1);
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, $args);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//
+//        $headers = ['Authorization: test_secret_key_b76acf9c6944411c90462f5fcc220707'];
+//        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+//
+//        // Response
+//        $response = curl_exec($ch);
+//        $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+//        curl_close($ch);
+//
+//        return $response;
+//    }
 
     public
     function checkout(Request $request)
