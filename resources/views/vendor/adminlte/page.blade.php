c7dbde9b75d348b630567047bd37eb7d885f11f2
ranjushrestha08 <ranjushrestha152@gmail.com>
2024-04-24 09:43:25 +0545

    update composer

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -15,9 +15,7 @@
     <div class="wrapper">
 
         {{-- Preloader Animation --}}
-        @if($layoutHelper->isPreloaderEnabled())
-            @include('adminlte::partials.common.preloader')
-        @endif
+     
 
         {{-- Top Navbar --}}
         @if($layoutHelper->isLayoutTopnavEnabled())
